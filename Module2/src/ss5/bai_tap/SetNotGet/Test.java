3a19b1b5d57a83f14ffe8fbbcd46a3d707a5ebc3
unknown <tuongminh7889@gmail.com>
2023-05-12 17:01:36 +0700

    bai2

---
+++ Module2/src/ss5/bai_tap/SetNotGet/Test.java
@@ -0,0 +1,9 @@
+package ss5.bai_tap.SetNotGet;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Minh");
+        student.setClasses("C03");
+    }
+}
