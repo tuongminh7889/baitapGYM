9917f620e5f2c8db76523611156bc1c0569c0129
unknown <tuongminh7889@gmail.com>
2023-05-12 16:19:43 +0700

    bai1

--- Module2/src/ss5/bai_tap/access_modifier/AccessModifier.java
+++ Module2/src/ss5/bai_tap/access_modifier/AccessModifier.java
@@ -1,28 +1,34 @@
 package ss5.bai_tap.access_modifier;
 
 public class AccessModifier {
-   private static double radius ;
-   private static String color ;
-   public final double PI = 3.14 ;
-   public  AccessModifier (){
+    private double radius;
+    private String color;
+    public final double PI = 3.14;
+
+    public AccessModifier() {
         radius = 1.0;
         color = "red";
-   }
-   public AccessModifier(double r){
-       this.radius = r ;
-   }
+    }
+
+    public AccessModifier(double r) {
+        this.radius = r;
+    }
 
     public double getRadius() {
         return this.radius;
     }
-    public double getArea(){
-       return (this.radius*this.radius) * PI;
+
+    public double getArea() {
+        return (this.radius * this.radius) * PI;
     }
 
-    public static void setColor(String color) {
-        AccessModifier.color = color;
+    public String setColor(String color) {
+       return this.color = color;
     }
 
+    public String getColor() {
+        return this.color;
+    }
 
 
 }
